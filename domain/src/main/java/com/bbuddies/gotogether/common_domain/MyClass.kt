7287e3613c125139e8324490ce9f828fac1562a5
artem.4e <maclaren9999@gmail.com>
2024-06-14 17:44:25 +0300

    Update dependencies & Gradle

---
+++ domain/src/main/java/com/bbuddies/gotogether/common_domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.bbuddies.gotogether.common_domain
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "GoTogether"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":presentation")
