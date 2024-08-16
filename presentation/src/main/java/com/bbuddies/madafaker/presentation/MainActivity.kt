ecd37f424f82aa564dc0b37af54de979ad57d526
Mura Keat <keatmur@gmail.com>
2024-08-16 19:43:43 +0300

    Retrieve the current registration token for debag

--- presentation/src/main/java/com/bbuddies/madafaker/presentation/MainActivity.kt
+++ presentation/src/main/java/com/bbuddies/madafaker/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bbuddies.madafaker.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +12,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.bbuddies.madafaker.presentation.ui.theme.MadafakerTheme
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+// Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+
+        })
         super.onCreate(savedInstanceState)
         setContent {
             MadafakerTheme {
